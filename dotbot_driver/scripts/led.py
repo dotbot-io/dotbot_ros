c14db2cb70c30e01fc9d2d189e0d62fb9aaf4f4e
Ludovico Russo <ludus.russo@gmail.com>
2016-03-21 23:21:37 +0100

    add led driver

--- dotbot_driver/scripts/led.py
+++ dotbot_driver/scripts/led.py
@@ -6,11 +6,17 @@ import RPi.GPIO as GPIO
 
 def led_cb(led):
     GPIO.output(7, led.led1)
+    GPIO.output(8, led.led2)
+    GPIO.output(9, led.led3)
 
 def led_driver():
     GPIO.setmode(GPIO.BOARD)
     GPIO.setup(7, GPIO.OUT)
+    GPIO.setup(8, GPIO.OUT)
+    GPIO.setup(9, GPIO.OUT)
     GPIO.output(7,False)
+    GPIO.output(8,False)
+    GPIO.output(9,False)
     rospy.init_node('led_driver', anonymous=True)
     rospy.Subscriber("/dotbot/led", Led, led_cb)
     rospy.spin()
