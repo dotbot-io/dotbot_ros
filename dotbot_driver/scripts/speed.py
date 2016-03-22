6a1530c950f200dafe7e5c9b6a0043b1a59f8458
Ludovico Russo <ludus.russo@gmail.com>
2016-03-22 23:58:57 +0100

    add speed node

--- dotbot_driver/scripts/speed.py
+++ dotbot_driver/scripts/speed.py
@@ -26,22 +26,6 @@ class SpeedNode():
         self.pwm_dx.start(msg.dx)
 
 
-def led_cb(led):
-    GPIO.output(led1, led.led1)
-    GPIO.output(led2, led.led2)
-    GPIO.output(led3, led.led3)
-
-def led_driver():
-    GPIO.setmode(GPIO.BOARD)
-    GPIO.setup(led1, GPIO.OUT)
-    GPIO.setup(led2, GPIO.OUT)
-    GPIO.setup(led3, GPIO.OUT)
-    GPIO.output(led1,False)
-    GPIO.output(led2,False)
-    GPIO.output(led3,False)
-    rospy.init_node('led_driver', anonymous=True)
-    rospy.Subscriber("/dotbot/led", Led, led_cb)
-    rospy.spin()
 
 if __name__ == '__main__':
-    led_driver()
+    n = SpeedNode()
