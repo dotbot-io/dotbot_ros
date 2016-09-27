53bbb273cf291846f2ddce8238d4c062ec0e27dc
Ludovico Russo <ludus.russo@gmail.com>
2016-09-27 13:54:02 +0200

    added input

---
+++ dotbot_driver/scripts/input.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+import rospy
+from dotbot_msgs.msg import Speed
+import RPi.GPIO as GPIO
+
+
+class InputNode():
+    def __init__(self, bs = (1)):
+        GPIO.setmode(GPIO.BOARD)
+        for b in bs:
+            GPIO.setup(dx[0], GPIO.IN)
+        rate = rospy.Rate(1) # 10hz
+
+        while not rospy.is_shutdown():
+            for b in bs:
+                print "input ", b, " : ", GPIO.input(b) 
+
+
+if __name__ == '__main__':
+    n = InputNode()
