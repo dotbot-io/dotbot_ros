bbc77af3456ed610fa9378c54c14f87768ef6243
Ludovico Russo <ludus.russo@gmail.com>
2016-09-27 14:10:40 +0200

    add Input msg

--- dotbot_driver/scripts/input.py
+++ dotbot_driver/scripts/input.py
@@ -1,19 +1,24 @@
 #!/usr/bin/env python
 import rospy
-from dotbot_msgs.msg import Speed
+from dotbot_msgs.msg import Input
 import RPi.GPIO as GPIO
 
 
 class InputNode():
     def __init__(self, bs = (3,5)):
+        pub = rospy.Publisher('input', Input, queue_size=10)
+        rospy.init_node('led_driver', anonymous=True)
         GPIO.setmode(GPIO.BOARD)
         for b in bs:
-            GPIO.setup(dx[0], GPIO.IN)
+            GPIO.setup(b, GPIO.IN)
         rate = rospy.Rate(1) # 10hz
 
         while not rospy.is_shutdown():
-            for b in bs:
-                print "input ", b, " : ", GPIO.input(b)
+            msg = Input()
+            msg.input1 = GPIO.input(bs[0])
+            msg.input2 = GPIO.input(bs[1])
+            pub.publish(msg)
+            rate.sleep()
 
 
 if __name__ == '__main__':
