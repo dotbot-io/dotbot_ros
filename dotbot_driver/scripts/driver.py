a8929609301754d18bdd9a67efd4b260dc3a1c09
Your Name <you@example.com>
2016-12-02 16:40:00 +0000

    up driver

--- dotbot_driver/scripts/driver.py
+++ dotbot_driver/scripts/driver.py
@@ -22,11 +22,13 @@ class DriverNode():
         GPIO.setup(dx[1], GPIO.OUT)
         GPIO.setup(sx[0], GPIO.OUT)
         GPIO.setup(sx[1], GPIO.OUT)
-        self.pwm_sx = GPIO.PWM(sx[0], 50)
-        self.pwm_dx = GPIO.PWM(dx[0], 50)
+        GPIO.setup(dx[0], GPIO.OUT)
+        GPIO.setup(dx[1], GPIO.OUT)
+        #self.pwm_sx = GPIO.PWM(sx[0], 100)
+        #self.pwm_dx = GPIO.PWM(dx[0], 100)
 
-        self.pin_dx = dx[1]
-        self.pin_sx = sx[1]
+        self.pin_dx = dx
+        self.pin_sx = sx
         rospy.Subscriber("speed", Speed, self.on_speed)
 
         self.stop_speed = 0
@@ -34,10 +36,10 @@ class DriverNode():
     def check_reset_speed(self):
         self.stop_speed += 1
         if self.stop_speed > 20:
-            GPIO.output(self.pin_dx, False)
-            GPIO.output(self.pin_sx, False)
-            self.pwm_sx.start(0)
-            self.pwm_dx.start(0)
+            GPIO.output(self.pin_dx[0], False)
+            GPIO.output(self.pin_sx[0], False)
+            GPIO.output(self.pin_dx[1], False)
+            GPIO.output(self.pin_sx[1], False)
 
     def init_leds(self, leds = (7,11,12)):
         self.leds = leds
@@ -64,18 +66,18 @@ class DriverNode():
     def on_speed(self, msg):
         self.stop_speed = 0
         if msg.sx < 0:
-            GPIO.output(self.pin_sx, True)
-            self.pwm_sx.start(msg.sx + 100)
+            GPIO.output(self.pin_sx[0], True)
+            GPIO.output(self.pin_sx[1], False)
         else:
-            GPIO.output(self.pin_sx, False)
-            self.pwm_sx.start(msg.sx)
+            GPIO.output(self.pin_sx[1], True)
+            GPIO.output(self.pin_sx[0], False)
 
         if msg.dx < 0:
-            GPIO.output(self.pin_dx, True)
-            self.pwm_dx.start(msg.dx + 100)
+            GPIO.output(self.pin_dx[0], True)
+            GPIO.output(self.pin_dx[1], False)
         else:
-            GPIO.output(self.pin_dx, False)
-            self.pwm_dx.start(msg.dx)
+            GPIO.output(self.pin_dx[1], True)
+            GPIO.output(self.pin_dx[0], False)
 
     def on_led(self, led):
         GPIO.output(self.leds[0], led.led1)
